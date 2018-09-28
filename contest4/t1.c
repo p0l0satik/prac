b05eff75d0a44852b65e22285a896174bbbee79a
p0l0satik <denis.cucu@yandex.ru>
2018-09-28 23:47:21 +0300

    modified after reject

--- contest4/t1.c
+++ contest4/t1.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <math.h>
+
 int 
 main(void)
 {
+    int n, w;
+    scanf("%o%o", &n, &w);
+    int st = pow(2, n);
+    for (int t = 0; t < st; t++) {
+        int pr = t % (st / 2);
+        if (t > st / 2) {
+            pr*= -1;
+        }
+        printf("|");
+        printf("%*o", w, t);
+        printf("|");
+        printf("%*d", w, t);
+        printf("|");
+        printf("%*d", w, pr);
+        printf("|");
+        printf("\n");
+        
+    }
     return 0;
 }
