0555155671e819935e8f5624993a71f44053aa38
p0l0satik <denis.cucu@yandex.ru>
2018-09-30 14:29:28 +0300

    explained constants, again.

--- contest3/t2.c
+++ contest3/t2.c
@@ -2,31 +2,17 @@
 #include <math.h>
 enum
 {
-    MAX_COURSE = 10000, 
-    ROUND_VAL = 10000,
-    MAX_CHANGE_ABS = 100,
+    MAX_COURSE_VALUE = 10000, 
+    ROUND_VALUE = 10000,
+    MAX_CHANGE_ABSOLUTE = 100,
     TO_PERCENT = 100 
 };
-    /*
-    MAX_COURSE:
-    The task says that course can not be greater then 10000. 
-    I use this constant to check if current course is less 
-    than the mentioned value.
-
-    ROUND_VAL:
-    According to the task the course must be rounded after each 
-    calculation according to the rules of math up to the 
-    fourth sign. To do this I multiply the course by 10000 then
-    round it up to the integer than divide it by 10000.
-
-    MAX_CHANGE_ABS:
-    The course can not be changed more that 100% of its value
-    this constant helps to check it.
-
-    TO_PERCENT:
-    While apllying changes to the course, I use this constant 
-    to divide the course change.
-    */
+/* 
+I am sorry, but I consider inappropriate to write comments 
+in Russian. That is why I decided to change contstants names.
+If they are not clear enough, please explain 
+what exactly is not OK, for me to fix it.
+*/
 
 int 
 main(int argc, char const *argv[])
@@ -35,7 +21,7 @@ main(int argc, char const *argv[])
     if (argc <= 1 || sscanf(argv[1], "%lf", &course) != 1) {
         return 1;
     }
-    if (course <= 0 || course > MAX_COURSE) {
+    if (course <= 0 || course > MAX_COURSE_VALUE) {
         return 1;
     }
     for (int t = 2; t < argc; t++) {
@@ -43,15 +29,15 @@ main(int argc, char const *argv[])
         if (sscanf(argv[t], "%lf", &j) != 1) {
             return 1;
         }
-        if (j >= MAX_CHANGE_ABS || j <= -MAX_CHANGE_ABS) {
+        if (j >= MAX_CHANGE_ABSOLUTE || j <= -MAX_CHANGE_ABSOLUTE) {
             return 1;
         }
         course += course * (j / TO_PERCENT);
-        course *= ROUND_VAL;
+        course *= ROUND_VALUE;
         course = round(course);
-        course /= ROUND_VAL;
+        course /= ROUND_VALUE;
 
-        if (course <= 0 || course > MAX_COURSE) {
+        if (course <= 0 || course > MAX_COURSE_VALUE) {
             return 1;
         }
     }
