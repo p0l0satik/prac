5cc10c85707368f95a16260b25a7d54cd67efad8
p0l0satik <denis.cucu@yandex.ru>
2018-10-04 14:48:37 +0300

    rejext fixed^ duplicate code deleted

--- mz5/up5_3.c
+++ mz5/up5_3.c
@@ -7,19 +7,8 @@ main(int argc, char const *argv[])
     FILE *f = fopen(argv[1], "r");
     time_t now, next;
     int year, month, day, hour, minut, sec;
-    if (fscanf(f, "%d/%d/%d %d:%d:%d", &year, &month, &day, &hour, &minut, &sec) != 6) {
-        return 0;
-    }
     struct tm date;
-    memset(&date, 0, sizeof(date));
-    date.tm_isdst = -1;
-    date.tm_year = year - 1900;
-    date.tm_mon = month - 1;
-    date.tm_mday = day;
-    date.tm_hour = hour;
-    date.tm_min = minut;
-    date.tm_sec = sec;
-    now = mktime(&date);
+    char flag = 0;
     while(fscanf(f, "%d/%d/%d %d:%d:%d", &year, &month, &day, &hour, &minut, &sec) == 6) {
         memset(&date, 0, sizeof(date));
         date.tm_isdst = -1;
@@ -30,7 +19,12 @@ main(int argc, char const *argv[])
         date.tm_min = minut;
         date.tm_sec = sec;
         next = mktime(&date);
-        printf("%ld\n", (long) next - now);
+        if (flag){
+            printf("%ld\n", (long) next - now);
+        } else {
+            flag = 1;
+        }
+        
         now = next;
     }
     return 0;   
